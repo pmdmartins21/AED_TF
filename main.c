75751c9c4e1cff628fa2f5d491cf9c387c6c76e5
Daniela <daniela_rgoncalves@hotmail.com>
2021-01-26 19:39:32 +0000

    Auto stash before merge of "main" and "origin/main"

--- main.c
+++ main.c
@@ -16,3 +16,33 @@ int main(int argc, char const *argv[])
     printf("===========MENU PRINCIPAL===========");
     return 0;
 }
+
+typedef struct Utente{
+    char nome[101];
+    int numeroDeUtente;
+    int idade;
+    int contactoTelefonico;
+    Vacina vacinaUtilizada;
+    int quantidadeDosesAdmn;
+    char dataUltimaDosagem[41];
+    
+    
+} CUtente;
+
+typedef struct sListaCentrosDeVacinacao
+{
+    CentroDeVacinacao centros[1000];
+    
+} ListaCentros;
+
+typedef struct sListaUtentes
+{
+    CUtente utentes[1000];
+
+} ListaUtentes;
+
+typedef struct sListaVacinas
+{
+    Vacina vacinas[10];
+
+} ListaVacinas;
