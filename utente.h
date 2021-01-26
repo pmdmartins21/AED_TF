c16d3181650b857db0efcd8995b58ab3190dc001
Pedro Martins <pmdmartins@gmail.com>
2021-01-26 19:52:41 +0000

    criacao utente e centros

--- utente.h
+++ utente.h
@@ -5,24 +5,11 @@ typedef struct sUtente{
     char nome[101];
     int numeroDeUtente;
     int idade;
-    int contactoTelefonico;
+    char contatoTelefonico[21];
     int vacinaID;
     int centroID;
     int quantidadeDosesAdmn;
     char dataUltimaDosagem[41];
 } Utente;
 
-Utente criarUtente() {
-    Utente u;
-    printf("O nome do Utente:");
-    while (getchar() != '\n');
-    fgets(u.nome,100, stdin);
-    u.nome[strlen(u.nome) -1 ] = '\0';
-    printf("O numero do utente :");
-    scanf("%d", &u.numeroDeUtente);
-    printf("Idade do utente :");
-    scanf("%d", &u.idade);
-    
-
-    return u;
-};
+Utente criarUtente();
