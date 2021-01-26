9916982a1d3082614b4e3f63e9f4136d07f6f487
Pedro Martins <pmdmartins@gmail.com>
2021-01-26 19:40:31 +0000

    dos mas

---
+++ centroVacinacao.h
@@ -0,0 +1,6 @@
+typedef struct sCentroVacinacao{
+    char nomeCentro[101];
+    char morada[101];
+    int idCentro;
+    
+} CentroVacinacao;
---
+++ vacina.h
@@ -0,0 +1,8 @@
+typedef struct sVacina{
+    char designacao[101];
+    int nDoses;
+    int tempoEntreVacinas;
+    int idVacina; //codigo
+   
+} Vacina;
+
