712d309f2f384e73047434e430bed7483e8d0716
Pedro Martins <pmdmartins@gmail.com>
2021-01-26 19:20:44 +0000

    strut

--- main.c
+++ main.c
@@ -2,29 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct sCentroDeVacinacao{
-    char nomeCentro[101];
-    char morada[101];
-    int idCentro;
-    
-} CentroDeVacinacao;
 
-typedef struct sVacina{
-    char designacao[101];
-    int nDoses;
-    int tempoEntreVacinas;
-    int idVacina; //codigo
-   
-} Vacina;
 
-typedef struct Utente{
-    char nome[101];
-    int numeroDeUtente;
-    int idade;
-    int contactoTelefonico;
-    Vacina vacinaUtilizada;
-    int quantidadeDosesAdmn;
-    char dataUltimaDosagem[41];
-    
-    
-} CUtente;
+
+int main(int argc, char const *argv[])
+{
+    printf("===========MENU PRINCIPAL===========");
+    printf("===========MENU PRINCIPAL===========");
+    printf("===========MENU PRINCIPAL===========");
+    printf("===========MENU PRINCIPAL===========");
+    printf("===========MENU PRINCIPAL===========");
+    printf("===========MENU PRINCIPAL===========");
+    printf("===========MENU PRINCIPAL===========");
+    return 0;
+}
