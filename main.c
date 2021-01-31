92f80c08da2357d04e2a23845bb03db987136e2d
Pedro Martins <pmdmartins@gmail.com>
2021-01-31 17:01:23 +0000

    MAIN

--- main.c
+++ main.c
@@ -41,6 +41,9 @@ int main(int argc, char const *argv[])
         printf("NÚMERO DE VACINAS ADMNISTRADAS\n");
         printf("MÉDIA DE IDADES DOS UTENTES VACINADOS\n");
         printf("VACINA %s (POR VACINA): NÚMERO DE UTENTES VACINADOS\n");
+        printf("1 - CENTROS\n\n");
+        printf("2 - UTENTES\n\n");
+        printf("4 - VACINAS\n\n");
         printf("4 - SAIR\n\n");
         printf("OPCAO: ");
 
@@ -69,42 +72,3 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-/*void tipo_registo(registos)
-{
-    ListaVacinas lv;
-    lv.numeroVacinas = 0;
-    ListaCentros lc;
-    int op=0;
-    ListaUtentes lu;
-
-    while(op!=4)
-    {
-        printf(">>> Tipo de registos <<<\n\n");
-        printf("1 - CENTROS\n");
-        printf("2 - UTENTES\n");
-        printf("3 - VACINAS\n");
-        printf("4 - VOLTAR\n\n");
-        printf("OPÇÃO: ");
-
-        scanf("%d", &op);
-        fflush(stdin);
-        switch(op)
-        {
-            case 1:
-                menuCentros(&lc);
-                break;
-            case 2:     
-                menuUtentes(&lu);
-                break;
-            case 3: 
-                menuVacinas(&lv);
-                break;
-            case 4: 
-                break;
-            default:
-                printf("Opção errada");
-                break;
-        }
-    }
-}
-*/
