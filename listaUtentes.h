899536e0579011edd3728eb8393057660de63497
Pedro Martins <pmdmartins@gmail.com>
2021-01-31 17:33:20 +0000

    listar utentes por vacina/centro

--- listaUtentes.h
+++ listaUtentes.h
@@ -16,5 +16,25 @@ void alterarContacto(ListaUtentes *lu, int idAEditar);
 void alterarIdadeUtente(ListaUtentes *lu, int idAEditar);
 void alterarNumeroUtente(ListaUtentes *lu, int idAEditar);
 
-void listarUtentesPorVacinas(ListaUtentes *lu);
+void listarUtentesPorVacinas(ListaUtentes *lu, ListaCentros *lc, ListaVacinas *lv);
+for (int i = 0; i < lv.numeroVacinas; i++) // correr lista vacinas
+{
+        printf("===== VACINA i =====")
+
+        for (int j = 0; j < lu.numeroUtentes; j++) // correr lista utentes e ver se o vacinaID do utente == i
+        {
+            if (lu.lu[j].vacinaID == i+1)
+            {
+                printf("Utente j")
+            }
+            
+        }
+            
+}
+
+printf("===== VACINA 1 =====")
+
+
+
+
 void listarUtentesPorCentro(ListaUtentes *lu);
